41abc0ab099b7100550c2b52d99954fd8808e391
Ruben Jenster <rjenster@gmail.com>
2014-02-03 20:50:44 +0100

    Added skeleton for mpd MQTT client.

---
+++ src/mpd-mqtt-client.c
@@ -0,0 +1,8 @@
+#include "mpd-mqtt-client.h"
+
+
+int main(int argc, char**argv)
+{
+
+  return 0;
+}
---
+++ src/mpd-mqtt-client.h
@@ -0,0 +1,8 @@
+#include <mosquitto.h>
+
+#include <mpd/client.h>
+#include <mpd/status.h>
+#include <mpd/song.h>
+#include <mpd/entity.h>
+#include <mpd/search.h>
+#include <mpd/tag.h>
